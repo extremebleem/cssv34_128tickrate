d3d217106c0add8cf08c8dea10692973916db98a
extremebleem <ivanxtragenius043@gmail.com>
2025-01-14 16:41:42 +1000

    initial

---
+++ metamod_128tickrate/metamod_128tickrate.cpp
@@ -0,0 +1,12 @@
+﻿// metamod_128tickrate.cpp: определяет точку входа для приложения.
+//
+
+#include "metamod_128tickrate.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
